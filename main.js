67a6d0f9c320a60e00a22eed67a0b3f6208ebd6c
Olezhabaget <kostoleh21@gmail.com>
2026-09-21 20:42:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const open1 = document.getElementById("open1");
+const close1 = document.getElementById("close1");
+const modal1 = document.getElementById("modal1");
+
+open1.addEventListener("click",() =>{
+modal1.style.display = "block";
+});
+
+close1.addEventListener("click",() =>{
+modal1.style.display = "none";
+});
+
+const open2 = document.getElementById("open2");
+const close2 = document.getElementById("close2");
+const modal2 = document.getElementById("modal2");
+
+open2.addEventListener("click",() =>{
+modal2.style.display = "block";
+});
+
+close2.addEventListener("click",() =>{
+modal2.style.display = "none";
+});
