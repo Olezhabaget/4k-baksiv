9584f05cb04631ebc25d1deb4d67ce5e22075a25
Olezhabaget <kostoleh21@gmail.com>
2026-09-22 19:01:02 +0200

    василівна

--- main.js
+++ main.js
@@ -21,3 +21,42 @@ modal2.style.display = "block";
 close2.addEventListener("click",() =>{
 modal2.style.display = "none";
 });
+
+const products = document.getElementById("products");
+
+fetch('./products.json')
+      .then(response => response.json())
+      .then(json => {
+        json.forEach(product => {
+            products.innerHTML+=`
+            <div class="product">
+                        <img src="${product.images[0]}" alt="img">
+                        <div>
+                            <div class="iamstar">
+                                <input id="star5" type="radio" name="star">
+                                <label for="star5">★</label>
+                                <input id="star4" type="radio" name="star">
+                                <label for="star4">★</label>
+                                <input id="star3" type="radio" name="star">
+                                <label for="star3">★</label>
+                                <input id="star2" type="radio" name="star">
+                                <label for="star2">★</label>
+                                <input id="star1" type="radio" name="star">
+                                <label for="star1">★</label>
+                            </div>
+
+                            <h3>${product.vidguki} відгуків</h3>
+                        </div>
+                        <p>Об'єм ${product.weight}</p>
+                        <h1>${product.brand}</h1>
+                        <h2>${product.name}</h2>
+                        <h4>${product.shortDescription}</h4>
+                        <div>
+                            <b>${product.price}</b>
+                            <s>${product.oldPrice}</s>
+                        </div>
+                        <button>Купити</button>
+                    </div>
+            `
+        });
+      })
